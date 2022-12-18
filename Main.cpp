dde48fbe28d74d6110e3cf7e7e8734e5d4c62727
tomolatoon <tomo2000tomo170@gmail.com>
2022-12-19 05:36:47 +0900

    add: centering the center rect

--- Main.cpp
+++ Main.cpp
@@ -11,6 +11,16 @@
 
 namespace tomolatoon
 {
+	double Sign(double d)
+	{
+		return (d > 0) - (d < 0);
+	}
+
+	double rSing(double d)
+	{
+		return -1 * Sign(d);
+	}
+
 	struct ListDrawer
 	{
 		struct IDrawer
@@ -52,6 +62,16 @@ namespace tomolatoon
 			return this->add(std::make_shared<LambdaWrapper>(std::move(lam)));
 		}
 
+		enum State : int16
+		{
+			//ButtonHandled, [[todo]]
+			MouseHandled    = 0b0000'0010, // マウスで動かしてる時
+			Coasting        = 0b0000'0100, // 惰性移動中
+			ToStoppingFirst = 0b0000'1000, // 止まり始めた最初の1フレーム
+			ToStopping      = 0b0001'0000, // 止まろうとしてる時
+			Stopping        = 0b0010'0000, // 完全に止まった時
+		};
+
 		void draw()
 		{
 			// [0, drawables.size() * height) に丸める
@@ -66,67 +86,138 @@ namespace tomolatoon
 				}
 			};
 
-			// 更新
+			// state 更新
 			if (Iframe::Rect().mouseOver() && !MouseL.down() && (MouseL.pressed() || MouseL.up()))
 			{
-				diff = rounding(diff + Cursor::DeltaF().y);
+				state = State::MouseHandled;
 			}
 			else
 			{
-				diff = rounding(diff);
-			}
-
-			// 中央に存在するカードのサイズ
-			int32  startIndex = drawables.size() - 1 - (int32)(diff / height);
-			// startIndex の描画を開始する位置(y座標)
-			double startY     = Iframe::Center().y + diff + height * startIndex;
-			// startIndex の描画位置(Rect)
-			Rect   startRect  = RectF{0, startY, Iframe::Width(), height}.asRect();
-
-			// [0, drawables.size()) に丸めながら増減させる
-			const auto inc = [&](int32& index) { index = index + 1 >= drawables.size() ? 0 : index + 1; };
-			const auto dec = [&](int32& index) { index = index - 1 < 0 ? drawables.size() - 1 : index - 1; };
-
-			// 中央のカード
-			{
-				ScopedIframe2D iframe(startRect);
-				drawables[startIndex].get()->draw(0.0);
-			}
-
-			// 上半分のカードたち
-			{
-				int32 index = startIndex;
-				Rect  rect  = startRect;
-
-				// 中央のカードは描かないので進めておく
-				rect.moveBy(0, -height), dec(index);
-
-				// 0 まで許容すると intersects の判定で false を食らうので 1 以上にしておく
-				for (; rect.bl().y > 0; rect.moveBy(0, -height), dec(index))
+				if (Abs(vel) < 50)
 				{
-					ScopedIframe2D iframe(rect);
-					drawables[index].get()->draw(0.0);
+					if (toStopping.isOne())
+					{
+						state = State::Stopping;
+					}
+					else
+					{
+						if (state != State::ToStoppingFirst && state != State::ToStopping)
+						{
+							state = State::ToStoppingFirst;
+						}
+						else
+						{
+							state = State::ToStopping;
+						}
+					}
+				}
+				else
+				{
+					state = State::Coasting;
 				}
 			}
 
-			// 下半分のカードたち
+			const auto toStoppingLerpCenterY = [&]() {
+				return Math::Lerp(toStoppingStart, toStoppingFinish, EaseOutQuint(toStopping.value()));
+			};
+
+			// 更新
+			switch (state)
 			{
-				int32 index = startIndex;
-				Rect  rect  = startRect;
+				using enum State;
+			case MouseHandled:
+			{
+				vel = Clamp(Cursor::DeltaF().y / Scene::DeltaTime(), -5000.0, 5000.0);
+			}
+			break;
+			case Coasting:
+			{
+				vel += rSing(vel) * 4000 * Scene::DeltaTime();
+			}
+			break;
+			case ToStoppingFirst:
+			{
+				toStoppingStart  = diff;
+				toStoppingFinish = rounding((drawables.size() - 1 - selected) * height + (double)height / 2);
+				vel              = 0;
+			}
+				[[fallthrough]];
+			case ToStopping:
+			{
+				toStoppingPrev = toStoppingLerpCenterY();
+				toStopping.update(true);
+			}
+			break;
+			case Stopping:
+				break;
+			}
 
-				// 中央のカードは描かないので進めておく
-				rect.moveBy(0, height), inc(index);
+			if (state & ~(State::ToStoppingFirst | State::ToStopping))
+			{
+				toStoppingStart  = 0.0;
+				toStoppingPrev   = 0.0;
+				toStoppingFinish = 0.0;
+				toStopping.update(false, 1.0);
+			}
 
-				for (; rect.tl().y < Iframe::Height(); rect.moveBy(0, height), inc(index))
+			// 実際に真ん中に据えるカード分の座標差
+			diff             = rounding(diff + vel * Scene::DeltaTime() + (toStoppingLerpCenterY() - toStoppingPrev));
+			// 中央に存在するカードのインデックス
+			selected         = drawables.size() - 1 - (int32)(diff / height);
+			// startIndex の描画を開始する位置(y座標)
+			double startY    = Iframe::Center().y + diff - height * (drawables.size() - selected);
+			// startIndex の描画位置(Rect)
+			Rect   startRect = RectF{0, startY, (double)Iframe::Width(), height}.asRect();
+
+			Print << U"vel: {}\ndiff:{}\nselected:{}\ncenterY:{}\ntoStoppingLerpCenterY():{}\ncenterRect:{}\ntoStoppingStart:{}\ntoStoppingPrev:{}\ntoStoppingFinish:{}\n"_fmt(vel, diff, selected, startY, toStoppingLerpCenterY(), startRect, toStoppingStart, toStoppingPrev, toStoppingFinish);
+
+			// 描画
+			{
+				// [0, drawables.size()) に丸めながら増減させる
+				const auto inc = [&](int32 index) { return index + 1 >= drawables.size() ? 0 : index + 1; };
+				const auto dec = [&](int32 index) { return index - 1 < 0 ? drawables.size() - 1 : index - 1; };
+
+				// 中央のカード
 				{
-					ScopedIframe2D iframe(rect);
-					drawables[index].get()->draw(0.0);
+					ScopedIframe2D iframe(startRect);
+					Iframe::Rect().draw(Palette::Azure);
+					//drawables[selected].get()->draw(EaseOutQuint(toStopping.value()));
+				}
+
+				// 上半分のカードたち
+				{
+					int32 index = dec(selected);
+					Rect  rect  = Rect{startRect.tl().movedBy(0, -height), Iframe::Width(), height};
+
+					// bl は Rect の場合下方向に 1px はみ出していると考えられるので、 0 は含まないでおく
+					for (; rect.bl().y > 0; rect.moveBy(0, -height), index = dec(index))
+					{
+						ScopedIframe2D iframe(rect);
+						drawables[index].get()->draw(EaseOutQuint(toStopping.value()));
+					}
+				}
+
+				// 下半分のカードたち
+				{
+					int32 index = inc(selected);
+					Rect  rect  = Rect{startRect.bl(), Iframe::Width(), height};
+
+					for (; rect.tl().y < Iframe::Height(); rect.moveBy(0, height), index = inc(index))
+					{
+						ScopedIframe2D iframe(rect);
+						drawables[index].get()->draw(EaseOutQuint(toStopping.value()));
+					}
 				}
 			}
 		}
 
 	private:
+		Transition                      toStopping{0.5s, 0.25s};
+		double                          toStoppingStart = 0, toStoppingPrev = 0, toStoppingFinish = 0;
+		State                           state     = State::Coasting; // 始めから停止させているとズレちゃうので、滑っていることにして停止するところからやる
+		int32                           selected  = 0;
 		int32                           height    = 100;
+		double                          vel       = 0.0;
 		double                          diff      = -(double)height / 2;
 		Array<std::shared_ptr<IDrawer>> drawables = {};
 	};
